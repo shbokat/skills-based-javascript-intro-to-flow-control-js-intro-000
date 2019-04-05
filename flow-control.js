cf4f71bbbb1d4646b50f4fe1c8e57cec6a4d8dd8
Spencer Bokat-Lindell <shbokat@gmail.com>
2019-04-05 17:02:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -37,6 +37,6 @@ function ternaryTeenager(age) {
     case age>=13 && age<=19:
     return "You are a teenager";
     default:
-    console.log"You have an age";
+    return "You have an age";
 }
 }
