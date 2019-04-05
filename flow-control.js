53a17528157d3ea2897b999f671acb638ccd3212
Spencer Bokat-Lindell <shbokat@gmail.com>
2019-04-05 17:14:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -34,7 +34,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
    switch (age) {
-    case age>12 && age<20:
+    case (age>12 && age<20):
       return "You are a teenager";
       break;
     default: return "You have an age";
