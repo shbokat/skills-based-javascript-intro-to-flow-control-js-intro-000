1e9c4615cd589776f0017d953e3d4bbcf90d8edd
Spencer Bokat-Lindell <shbokat@gmail.com>
2019-04-05 16:57:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -34,25 +34,7 @@ function ternaryTeenager(age) {
 
   function switchAge(age) {
     switch(age){
-    case 13:
-    console.log("You are a teenager");
-    break;
-    case 14:
-    console.log("You are a teenager");
-    break;
-    case 15:
-    console.log("You are a teenager");
-    break;
-    case 16:
-    console.log("You are a teenager");
-    break;
-    case 17:
-    console.log("You are a teenager");
-    break;
-    case 18:
-    console.log("You are a teenager");
-    break;
-    case 19:
+    case age>=13 && age<=19:
     console.log("You are a teenager");
     break;
     default:
