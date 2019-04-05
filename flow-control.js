bcb6860af7df99a661f5c9403ef8dd71cbca4210
Spencer Bokat-Lindell <shbokat@gmail.com>
2019-04-05 16:49:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -36,10 +36,10 @@ function switchAge(age) {
   case (age>=13 && age<=19):
   console.log("You are a teenager");
   break;
-  default: 
+  default:
   console.log("You have an age");
 }
-  
-  
+
+
 
 }
